e49d0b4f2cd3ee8ffe423214c415da9ccadf2f80
vasconevexd <vasco.neves98@gmail.com>
2020-11-11 13:18:19 +0000

    Update README and adjust code

---
+++ README.md
@@ -0,0 +1,12 @@
+# InstagramFollowing
+
+Python script to gain more followers!!
+
+
+
+## Requirements
+------
+
+* [Python 3](https://www.python.org/download/releases/3.0/)
+* [Selenium](https://selenium-python.readthedocs.io/)
+
--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from selenium.common.exceptions import NoSuchElementException
 from time import sleep
 
 
-
 class InstaBot:
     def __init__(self, username, pw):
         self.driver = webdriver.Chrome(executable_path='C:\chromedriver\chromedriver.exe')
@@ -21,110 +20,104 @@ class InstaBot:
         self.driver.find_element_by_xpath('//button[@type="submit"]')\
             .click()
         sleep(4)
-       
 
-    def get_unfollowers(self,username):
+    def get_unfollowers(self, username):
 
         self.driver.get("https://instagram.com/" + username)
         sleep(2)
         self.driver.find_element_by_xpath("//a[contains(@href,'/following')]")\
             .click()
-        self._get_names()
-       
+        self._get_names(username)
 
-
-    def _get_names(self):
+    def _get_names(self, username):
 
         sleep(2)
 
         user_path = self.driver.find_elements_by_css_selector('.PZuss > li')
-        print(user_path)
         numb_users_class = len(user_path)
-        
 
         for x in range(numb_users_class):
-            
+
             x += 1
 
             if numb_users_class-1 == x:
-                
-                user_path = self.driver.find_elements_by_css_selector('.PZuss > li')
+
+                user_path = self.driver.find_elements_by_css_selector(
+                    '.PZuss > li')
                 numb_users_class = len(user_path)
 
-            elem = self.driver.find_element_by_css_selector('.PZuss > li:nth-child(' + str(x) + ') a')
+            elem = self.driver.find_element_by_css_selector(
+                '.PZuss > li:nth-child(' + str(x) + ') a')
 
             if elem.is_displayed():
- 
-                self.driver.execute_script('arguments[0].scrollIntoView(true);', elem)
+
+                self.driver.execute_script(
+                    'arguments[0].scrollIntoView(true);', elem)
                 sleep(4)
-                print("=>Index: " + str(x) + "Valor Total" + str(numb_users_class))
+
                 self.driver.find_element_by_css_selector('.PZuss > li:nth-child(' + str(x) + ') a')\
-                .click()
+                    .click()
                 print("Element found")
-                   
+
             else:
                 print("Element not found")
-                    
+
             sleep(3)
 
-            
             self.driver.find_element_by_xpath("//a[contains(@href,'/following')]")\
                 .click()
-            
-            sleep(2)
-           
-       
-            
-            numb_following, y = 0, 0
-            
-            user_path = self.driver.find_elements_by_css_selector('.PZuss > li')
-            numb_following = len(user_path)
-            print(numb_following)
-                
-            sleep(2)
-                
 
-            for y in range(numb_following):          
-                
-                y += 1  
+            sleep(2)
+
+            numb_following, y = 0, 0
+
+            user_path = self.driver.find_elements_by_css_selector(
+                '.PZuss > li')
+            numb_following = len(user_path)
+
+            sleep(2)
+
+            for y in range(numb_following):
+
+                y += 1
 
                 if numb_following-1 == y:
-                    user_path = self.driver.find_elements_by_css_selector('.PZuss > li')
+                    user_path = self.driver.find_elements_by_css_selector(
+                        '.PZuss > li')
                     numb_following = len(user_path)
-               
 
                 try:
 
-                    element = self.driver.find_element_by_css_selector('.PZuss > li:nth-child(' + str(y) + ') .sqdOP.L3NKy.y3zKF')
+                    element = self.driver.find_element_by_css_selector(
+                        '.PZuss > li:nth-child(' + str(y) + ') .sqdOP.L3NKy.y3zKF')
 
                     if element.is_displayed():
- 
-                        self.driver.execute_script('arguments[0].scrollIntoView(true);', element)
+
+                        self.driver.execute_script(
+                            'arguments[0].scrollIntoView(true);', element)
                         sleep(4)
-                        print("=>Index: " + str(y) + "Valor Total" + str(numb_following))
+
                         self.driver.find_element_by_css_selector('.PZuss > li:nth-child(' + str(y) + ' ) button')\
-                        .click()
+                            .click()
                         print("Element found")
-                   
+
                     else:
                         print("Element not found")
-                    
+
                         sleep(3)
 
                 except NoSuchElementException:
                     print("No element found")
-                
-              
-            sleep(2)    
-            
-            self.driver.get("https://instagram.com/username")
-                
+
+            sleep(2)
+
+            self.driver.get("https://instagram.com/" + username)
+
             sleep(3)
 
             self.driver.find_element_by_xpath("//a[contains(@href,'/following')]")\
                 .click()
             sleep(3)
-                            
 
 
 my_bot = InstaBot('username', '1231234')
